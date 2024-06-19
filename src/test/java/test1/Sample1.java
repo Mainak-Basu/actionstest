822155dfc0a60f1030cb7c8b15dac3fa37d65242
mainak basu <mainakbasu4@gmail.com>
2024-06-19 15:38:42 +0530

    t1

---
+++ src/test/java/test1/Sample1.java
@@ -0,0 +1,35 @@
+package test1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Sample1 {
+	private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu"); // applicable to Windows OS
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void testExample() {
+        driver.get("https://www.google.co.in/");
+        System.out.println("Title: " + driver.getTitle());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
